51ae96fe9a5410242d631fafbdb86c679cfb1083
Mike <mjwalshe@gmail.com>
2019-09-19 15:42:38 +0100

    Add minor interface styling

---
+++ src/jquery.js
@@ -0,0 +1,36 @@
+runJQuery = function() {
+
+  $(document).ready(function() {
+
+    thermostat = new Thermostat();
+
+    function UpdatePage(){
+      $("#energy-usage-display").css({color: thermostat.energyUsage()});
+      $("#temperature").html(thermostat._temperature + '&#176 C');
+      $("#power-saving-display").css({color: thermostat._powerSavingOn})
+    };
+
+    UpdatePage();
+    
+    $('#power-saving-display').on('click', function(){
+      thermostat.togglePowerSaving();
+      UpdatePage();
+    });
+
+    $('#increment').on('click', function(){
+      thermostat.increment();
+    UpdatePage ();
+    });
+
+    $('#decrement').on('click', function(){
+      thermostat.decrement();
+    UpdatePage ();
+    });
+
+    $('#reset').on('click', function(){
+      thermostat.reset();
+    UpdatePage ();
+    });
+
+  })
+}
